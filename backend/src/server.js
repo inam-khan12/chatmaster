85e818cba58b8eea4dcb0aeb0c7cc0989825771d
Inam Niazi <inamkhan51272@gmail.com>
2026-07-07 11:56:33 +0500

    sign up page created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,37 +1,46 @@
-import express from "express"
+import express from "express";
 import path from "path";
-import cookieparser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
 
 const app = express();
 
 const __dirname = path.resolve();
-
 const PORT = ENV.PORT || 3000;
+
+// Middleware
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: ENV.CLIENT_URL,
+    credentials: true,
+  })
+);
 
+app.use(cookieParser());
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use(cookieparser());
 
-//make eay for deployment 
-
+// Deployment
+if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
+}
 
 console.log(ENV.NODE_ENV);
 
-app.listen(3000,()=>{
-  console.log("app is running on port 3000")
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
   connectDB();
-} )
+});
